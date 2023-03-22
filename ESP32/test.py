76ac53ec3bc4d624448999ef430bce1c861c5ba3
Q Zhang <qz108@georgetown.edu>
2023-03-22 18:19:47 -0400

    Update with design and some extra readmes

--- ESP32/test.py
+++ ESP32/test.py
@@ -1,10 +1,11 @@
+# This script continually reads from the sensors
 from sensors.bh1750 import BH1750
 from sensors.sht30 import SHT30
 from time import sleep
 from machine import Pin, SoftI2C
 i2c = SoftI2C(scl=Pin(35), sda=Pin(33))
-light = BH1750(i2c)
-th = SHT30(i2c)
+light = BH1750(i2c) # light sensor (lumens)
+th = SHT30(i2c) # temperature & humidity sensor
 while True: 
 	i2c.scan()
 	temperature, humidity = th.measure()
---
+++ design/README.md
@@ -0,0 +1,3 @@
+# FloraFlow Design
+
+For more in-depth discussion and brainstorming, see FloraFlow Google Drive.
